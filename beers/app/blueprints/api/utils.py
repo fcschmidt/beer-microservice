51c7a2c8de32940f4179ef1dbcce2443d6824905
fcschmidt <fcschimidt@gmail.com>
2019-02-17 21:20:27 -0300

    Created Ingredients serializer

--- beers/app/blueprints/api/utils.py
+++ beers/app/blueprints/api/utils.py
@@ -1,9 +1,17 @@
-from beers.app.blueprints.api.schemas import Beers
+from beers.app.blueprints.api.schemas import BeerSchema, BeerIngredientsSchema
 
 
-beers_schema = Beers(strict=True)
+beers_schema = BeerSchema(strict=True)
 
 
 def beers_serializer(content):
     serialized = beers_schema.dump(content, many=True).data
+    return serialized
+
+
+ingredients_schema = BeerIngredientsSchema(strict=True)
+
+
+def ingredients_serializer(content):
+    serialized = ingredients_schema.dump(content, many=True).data
     return serialized
