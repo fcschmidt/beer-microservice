bf4e3ba3c3f3c75efc4b6cd92371c6fe8980c7b5
fcschmidt <fcschimidt@gmail.com>
2019-02-18 16:45:18 -0300

    Added message

--- beers/app/blueprints/api/errors.py
+++ beers/app/blueprints/api/errors.py
@@ -1,7 +1,7 @@
 from flask_restful import abort
 
 
-def error_does_not_exist(resource):
+def error_does_not_exist(resource, msg):
     """Response 404 Not Found"""
     if not resource:
-        abort(http_status_code=404, error=f"Beer does not exist.")
+        abort(http_status_code=404, error=f"{msg} does not exist.")
