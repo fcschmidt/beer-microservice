8b1b80f805fb90a13fddd5aa0c4021e6547969f2
fcschmidt <fcschimidt@gmail.com>
2019-02-18 11:38:18 -0300

    Created responses

---
+++ beers/app/blueprints/api/responses.py
@@ -0,0 +1,44 @@
+from flask import jsonify
+
+
+def resp_successfully(content):
+    """Response 200"""
+    response = jsonify(content)
+    response.status_code = 200
+    return response
+
+
+def resp_not_items():
+    """Response 200"""
+    response = jsonify({
+        'message': 'Data base empty!'
+    })
+    response.status_code = 200
+    return response
+
+
+def resp_create_successfully():
+    """Response 201"""
+    response = jsonify({
+        'message': 'Beer create successfully!'
+    })
+    response.status_code = 201
+    return response
+
+
+def resp_update_successfully():
+    """Response 201"""
+    response = jsonify({
+        'message': 'Beer update successfully!'
+    })
+    response.status_code = 201
+    return response
+
+
+def resp_delete_successfully():
+    """Response 202"""
+    response = jsonify({
+        'message': 'Beer delete successfully!'
+    })
+    response.status_code = 202
+    return response
