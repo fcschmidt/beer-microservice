20eb1fb27cdfd770b67ee96285821ade85981a70
fcschmidt <fcschimidt@gmail.com>
2019-02-20 15:48:07 -0300

    Added new extension, for handling Cross Origin Resource Sharing

--- beers/app/app_factory.py
+++ beers/app/app_factory.py
@@ -1,9 +1,11 @@
 from beers.settings import app_config
 from flask import Flask
+from flask_cors import CORS
 
 
 def create_app(config_name):
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(app_config[config_name])
 
     # Initialize Extensions
