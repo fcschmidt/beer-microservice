70380321a9099baa13db8092cf67ed59afa44285
fcschmidt <fcschimidt@gmail.com>
2019-02-22 11:35:08 -0300

    Added route for beer edit

--- beers-app/web/src/app/app-routing.module.ts
+++ beers-app/web/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { BeersListComponent} from "./beers-list/beers-list.component";
 import { BeerDetailComponent} from "./beer-detail/beer-detail.component";
+import { BeerEditComponent } from "./beer-edit/beer-edit.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/cervejas', pathMatch: 'full' },
   { path: 'cervejas', component: BeersListComponent },
-  { path: 'cervejas/:id/:beer_name', component: BeerDetailComponent }
+  { path: 'cervejas/:id/:beer_name', component: BeerDetailComponent },
+  { path: 'cervejas/:id/:beer_name/edit', component: BeerEditComponent}
 
 ];
 
