9777b9a917be2e6e4c95b74796814b6b945be667
fcschmidt <fcschimidt@gmail.com>
2019-02-22 14:46:02 -0300

    Create new method (Search Beer method)

--- beers-app/web/src/app/beer.service.ts
+++ beers-app/web/src/app/beer.service.ts
@@ -14,16 +14,25 @@ const httpOptions = {
 export class BeerService {
 
   private apiUrl = 'http://localhost:5000/api/v1/beers';
+  private apiSearchUrl = 'http://localhost:5000/api/v1/beers/search?search=';
 
   constructor(private http: HttpClient) { }
 
+  /* GET: */
   public getBeers(): Observable<Beer[]>{
     return this.http.get<Beer[]>(this.apiUrl)
   }
 
+  /* GET: */
   public getBeer(beerId: number): Observable<Beer>{
     const url =`${this.apiUrl}/${beerId}`;
     return this.http.get<Beer>(url)
+  }
+
+  /* GET: */
+  public searchBeer(search: string): Observable<Beer>{
+    const searchUrl = `${this.apiSearchUrl}${search}`;
+    return this.http.get<Beer>(searchUrl)
   }
 
   // Methods
@@ -35,5 +44,7 @@ export class BeerService {
 
 
   /* DELETE: Delete the beer from the server */
+
+
 
 }
