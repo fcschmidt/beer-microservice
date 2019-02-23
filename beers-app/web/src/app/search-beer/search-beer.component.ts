17549811abb05fd1f8d516d837a24b3d81c7717c
fcschmidt <fcschimidt@gmail.com>
2019-02-22 21:09:11 -0300

    Create new Component

---
+++ beers-app/web/src/app/search-beer/search-beer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-beer',
+  templateUrl: './search-beer.component.html',
+  styleUrls: ['./search-beer.component.css']
+})
+export class SearchBeerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
