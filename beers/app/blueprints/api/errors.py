14f34e6dea1cbb79574276bfef01ec633d8db5ac
fcschmidt <fcschimidt@gmail.com>
2019-04-13 19:19:15 -0300

    Revomed Search error

--- beers/app/blueprints/api/errors.py
+++ beers/app/blueprints/api/errors.py
@@ -1,5 +1,5 @@
 from flask_restful import abort
-from flask import jsonify
+# from flask import jsonify
 
 
 def error_does_not_exist(resource, msg):
@@ -8,10 +8,10 @@ def error_does_not_exist(resource, msg):
         abort(http_status_code=404, error=f"{msg} does not exist.")
 
 
-def error_not_inserted():
-    """Response 404"""
-    response = jsonify({
-        'error': 'No items inserted in the search!'
-    })
-    response.status_code = 404
-    return response
+# def error_not_inserted():
+#     """Response 404"""
+#     response = jsonify({
+#         'error': 'No items inserted in the search!'
+#     })
+#     response.status_code = 404
+#     return response
