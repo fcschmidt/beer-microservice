5c2cff82dc3237de5817bd5e133aeb25ef26e538
fcschmidt <fcschimidt@gmail.com>
2019-04-14 18:06:56 -0300

    Added response json equals expected

--- beers/tests/test_ingredients_api_resources.py
+++ beers/tests/test_ingredients_api_resources.py
@@ -10,6 +10,12 @@ def test_create_new_ingredient_not_exist_beer_id(client, session):
     response = client.post('/api/v1/ingredients', json=new_ingredient)
     assert response.status_code == 404
 
+    expected = {
+        'error': '10 does not exist.'
+    }
+    resp_json = response.get_json()
+    assert resp_json['error'] == expected['error']
+
 
 def test_create_new_ingredient_already_exists(client, session):
     populate_beers(3)
@@ -35,3 +41,9 @@ def test_create_new_ingredient(client, session):
     }
     response = client.post('/api/v1/ingredients', json=new_ingredient)
     assert response.status_code == 201
+
+    expected = {
+        'message': 'Ingredient added successfully!'
+    }
+    resp_json = response.get_json()
+    assert resp_json['message'] == expected['message']
