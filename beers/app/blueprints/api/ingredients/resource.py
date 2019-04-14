ad5a15b774871207287e4dbe191585310f249e76
fcschmidt <fcschimidt@gmail.com>
2019-04-14 17:51:34 -0300

    Created new resource and respective tests
    close #3

--- beers/app/blueprints/api/ingredients/resource.py
+++ beers/app/blueprints/api/ingredients/resource.py
@@ -1,14 +1,21 @@
 from flask import Blueprint
 from flask_restful import Api, reqparse, fields, Resource
 
+from beers.app.blueprints.api.models.ingredients_model import BeerIngredients as IngredientsModel
+
+from beers.app.blueprints.api.errors import error_does_not_exist, error_already_exists
+from beers.app.blueprints.api.responses import resp_ingredients_create_successfully
+
+from beers.app.blueprints.api.utils import ingredients_serializer
 
 bp = Blueprint('ingredients_api', __name__, url_prefix='/api/v1')
 api = Api(bp)
 
 ingredients_parser = reqparse.RequestParser()
 ingredients_parser.add_argument('ingredient_name', type=str)
+ingredients_parser.add_argument('beer_id', type=int)
 
-resource_fields_ingredients = {
+resource_fields = {
     'id': fields.Integer,
     'ingredient_name': fields.String,
     'beer_id': fields.Integer,
@@ -16,9 +23,31 @@ resource_fields_ingredients = {
 
 
 class Ingredients(Resource):
-    pass
+
+    def __init__(self):
+        self.ingredients_args = ingredients_parser.parse_args()
+
+    def post(self):
+        ingredient_name = self.ingredients_args['ingredient_name']
+        beer_id = self.ingredients_args['beer_id']
+
+        query_filter = IngredientsModel.filter_beer_id(beer_id)
+        if not query_filter:
+            error_does_not_exist(None, beer_id)
+
+        serialized = ingredients_serializer(query_filter, True)
+        for item in serialized:
+            if item['ingredient_name'] == ingredient_name:
+                return error_already_exists()
+
+        ingredients = IngredientsModel(
+            ingredient_name=ingredient_name,
+            beer_id=beer_id
+        )
+        ingredients.save()
+        return resp_ingredients_create_successfully()
 
 
 def init_app(app):
-    api.resource(Ingredients, '/ingredients', endpoint='ingredients')
+    api.add_resource(Ingredients, '/ingredients', endpoint='ingredients')
     app.register_blueprint(bp)
--- beers/tests/test_ingredients_api_resources.py
+++ beers/tests/test_ingredients_api_resources.py
@@ -1,5 +1,37 @@
 from beers.tests.scripts.populate_data_base import populate_beers
 
 
+def test_create_new_ingredient_not_exist_beer_id(client, session):
+    populate_beers(3)
+    new_ingredient = {
+        'ingredient_name': 'aveia',
+        'beer_id': 10
+    }
+    response = client.post('/api/v1/ingredients', json=new_ingredient)
+    assert response.status_code == 404
+
+
+def test_create_new_ingredient_already_exists(client, session):
+    populate_beers(3)
+    new_ingredient = {
+        'ingredient_name': 'cevada',
+        'beer_id': 1
+    }
+    response = client.post('/api/v1/ingredients', json=new_ingredient)
+    assert response.status_code == 400
+
+    expected = {
+        'error': 'An ingredient with this name already exists.'
+    }
+    resp_json = response.get_json()
+    assert resp_json['error'] == expected['error']
+
+
 def test_create_new_ingredient(client, session):
-    pass
+    populate_beers(3)
+    new_ingredient = {
+        'ingredient_name': 'mirtilo',
+        'beer_id': 1
+    }
+    response = client.post('/api/v1/ingredients', json=new_ingredient)
+    assert response.status_code == 201
