1963cea1007d7433597a521df5b9a9a7b817fa9c
fcschmidt <fcschimidt@gmail.com>
2019-04-14 17:49:32 -0300

    Created new error message

--- beers/app/blueprints/api/errors.py
+++ beers/app/blueprints/api/errors.py
@@ -1,5 +1,5 @@
 from flask_restful import abort
-# from flask import jsonify
+from flask import jsonify
 
 
 def error_does_not_exist(resource, msg):
@@ -7,6 +7,14 @@ def error_does_not_exist(resource, msg):
     if not resource:
         abort(http_status_code=404, error=f"{msg} does not exist.")
 
+
+def error_already_exists():
+    """Response 400"""
+    response = jsonify({
+        'error': 'An ingredient with this name already exists.'
+    })
+    response.status_code = 400
+    return response
 
 # def error_not_inserted():
 #     """Response 404"""
