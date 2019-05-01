917130dcf279cf2dd00c97c4c4474b6ceb9441b1
fcschmidt <fcschimidt@gmail.com>
2019-05-01 10:31:16 -0300

    Imported Ingredient Create component

--- beers-app/web/src/app/app.module.ts
+++ beers-app/web/src/app/app.module.ts
@@ -4,14 +4,15 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from "@angular/forms";
 
-import {BeerService} from "./beer.service";
-import {IngredientService} from "./ingredient.service";
+import { BeerService } from "./beer.service";
+import { IngredientService } from "./ingredient.service";
 import { AppComponent } from './app.component';
 import { BeersListComponent } from './beers-list/beers-list.component';
 import { BeerDetailComponent } from './beer-detail/beer-detail.component';
 import { BeerEditComponent } from './beer-edit/beer-edit.component';
 import { SearchBeerComponent } from './search-beer/search-beer.component';
 import { BeerCreateComponent } from './beer-create/beer-create.component';
+import { IngredientCreateComponent } from './ingredient-create/ingredient-create.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { BeerCreateComponent } from './beer-create/beer-create.component';
     BeerEditComponent,
     SearchBeerComponent,
     BeerCreateComponent,
+    IngredientCreateComponent,
   ],
   imports: [
     BrowserModule,
