92c516717b508a950bd3c821c042c5312dc4f21f
fcschmidt <fcschimidt@gmail.com>
2019-05-01 10:25:11 -0300

    Created Deleted method: for delete ingredient

--- beers-app/web/src/app/ingredient.service.ts
+++ beers-app/web/src/app/ingredient.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http";
+import { Observable } from "rxjs";
+// import { map } from "rxjs/operators";
+
+import { Ingredient } from "./ingredient";
 
 @Injectable({
   providedIn: 'root'
 })
 export class IngredientService {
 
-  constructor() { }
+  private API_URL = 'http://localhost:5000/api/v1/ingredients';
 
+  constructor(private http: HttpClient) { }
+
+  /*POST: Add a new ingredient to the server. */
+
+  /*DELETE: Delete the ingredient from the server. */
+  deleteIngredient(ingredient): Observable<Ingredient>{
+    const url = `${this.API_URL}/${ingredient.id}`;
+    return this.http.delete<Ingredient>(url);
+  }
 }
